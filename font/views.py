fc250daaaf4e62cf9abd577f6dcd57f0de08ce49
star_mac <lxc902@qq.com>
2013-12-22 03:13:44 +0800

    'init register'

--- font/views.py
+++ font/views.py
@@ -1,4 +1,5 @@
-from django.http import *
+from models import *
+from django.shortcuts import *
 
 
 def get_para(request):
@@ -11,19 +12,40 @@ def get_para(request):
 
 
 def check_para(p, needs):
-    ret = ''
+    error = ''
     for need in needs:
         if need not in p:
-            ret += need + ' is required! '
-    return ret
+            error += need + ' is required! '
+    return error
 
 
-def handle_error(error):
-    
+def handle_error(request, error):
+    return render_to_response('error.html', {'error_info':error, 'back_url':request.path})
 
 
-def view_register(request):
+def register_btn(request):
+    p = get_para(request)
+    error = check_para(p, ['username', 'password', 'email'])
+    if error:
+        return handle_error(request, error)
+
+    res = Users.objects.filter(username=p['username'])
+    if res:
+        return handle_error(request, 'duplicate username')
+
+    user = Users.objects.create(
+        username = p['username'],
+        password = p['password'],
+        email = p['email'],
+        is_active = False
+    )
+    if not user:
+        return handle_error(request, "can't create user! unknown reason")
+    return HttpResponseRedirect('/login/')
+
+
+def login_btn(request):
     p = get_para(request)
     error = check_para(p, ['username', 'password'])
     if error:
-        return handle_error(error)
+        return handle_error(request, error)
